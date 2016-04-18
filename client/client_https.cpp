3bccff06e830867ef030a0a60e968c85aeacd06b
Andy Medina <andymedina@utexas.edu>
2016-04-18 08:55:41 -0500

    comments

--- client/client_https.cpp
+++ client/client_https.cpp
@@ -67,42 +67,42 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-
+    //initialize math libraries
     initAnonize();
     cout<<"initAnonize()"<<endl;
     string uid;
     //Client examples
     //Second Client() parameter set to false: no certificate verification
     HttpsClient client(host+":8080", false);
+
+    //input username id
     cout << "Register your UserName: ";
-    // cin >> uid;
     cin >> uid;
-    // auto r1=client.request("GET", "/match/123");
-    // cout << r1->content.rdbuf() << endl;
 
-
+    //retrieve RA public key and register UID
     stringstream ss;
     auto r1=client.request("POST", "/registerUser", uid);
     ss << r1->content.rdbuf();
     string RAVK=ss.str();
 
+
     const char* precred = makeCred(uid.c_str());
     string reg1 = registerUserMessage(precred, RAVK.c_str());
 
+    //retreive survey registration confirmation
     stringstream ss2;
     string json_string="{\"uid\": \"" + uid + "\",\"reg\": \"" + reg1 + "\"}";
     auto r2=client.request("POST", "/registerServerResponse", json_string);
     ss2 << r2->content.rdbuf();
     string reg2=ss2.str();
-
+    //final user credential
     const char* cred = registerUserFinal(uid.c_str(), reg2.c_str(), precred, RAVK.c_str());
 
+    //retrieve survey id and public key and parse json
     auto cred_json =client.request("POST", "/surveyCred", uid);
-
-
     ptree pt;
-    read_json(cred_json->content, pt);
 
+    read_json(cred_json->content, pt);
     string uidsig = pt.get<string>("uidsig");
     string vid = pt.get<string>("vid");
     string vk = pt.get<string>("vk");
@@ -111,36 +111,39 @@ int main(int argc, char *argv[]) {
     cout << "vid   : " << vid << endl;
     cout << "vk    : " << vk << endl;
 
-    string msg = "[", finalmsg;
+    string msg = "[", finalmsg, answer;
 
     auto survey_json =client.request("GET", "/survey");
     ptree survey;
     read_json(survey_json->content, survey);
 
-    string answer;
     cin.ignore();
+
+    //loop through json array of questions
     for (auto i : as_vector<string>(survey, "questions")) {
         cout << i << "\n";
         getline(cin, answer);
         msg = msg + "\"" + answer + "\",";
     }
 
+    //delete leading comma and close msg
     msg.erase(msg.size() - 1);
     msg+="]";
+
 
     cout << "\nmsg : " << msg << endl;
 
     // msg = "reeaeeeeeeeeeeeeeeeeeeeeeeeeeeeallylongmessagethatwillbelong";
 
+    //compute final message string for submission
     finalmsg = submitMessage(msg.c_str(), cred, RAVK.c_str(), uidsig.c_str(), vid.c_str(), vk.c_str());
     boost::replace_all(finalmsg, "\n", "~");
 
-    cout << "finalmessage: \n" << finalmsg << endl;
 
+    cout << "finalmessage: \n" << finalmsg << endl;
     cout << "size: " << finalmsg.size() << endl;
 
-    
-
+    //submit survey results
     auto submit_response =client.request("POST", "/submit", finalmsg);
     cout << "Submit Response : " << submit_response->content.rdbuf() << endl;
 
